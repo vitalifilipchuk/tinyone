9fe7f5c88bd02ba2c87c9f37c011154b78e356ea
vitaliifilipchuk <vitali.filipchuk@gmail.com>
2019-01-30 18:39:29 +0200

    basic commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var map = require('gulp-sourcemaps');
+var prefixer = require('gulp-autoprefixer');
+
+gulp.task('css', function() {
+   return gulp.src('src/css/style.scss')
+   				.pipe(map.init())
+   			  	.pipe(sass())
+   			  	.pipe(prefixer())
+   		      	.pipe(cleanCSS({compatibility: 'ie8'}))
+   		      	.pipe(map.write())
+              	.pipe(gulp.dest('build/css'));
+});
